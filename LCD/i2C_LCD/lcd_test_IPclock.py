b993354b5d59f6b4e15376ae7be832984b1445f0
Glyn Hudson <glyn.hudson@gmail.com>
2014-06-01 13:29:29 +0000

    add LCD i2c test

---
+++ LCD/i2C_LCD/lcd_test_IPclock.py
@@ -0,0 +1,32 @@
+import lcddriver
+from subprocess import *
+from time import sleep, strftime
+from datetime import datetime
+
+lcd = lcddriver.lcd()
+
+
+lcd.lcd_display_string("emonPi", 1)
+lcd.lcd_display_string("IP clock test",2)  
+sleep(1)
+ 
+cmd = "ip addr show wlan0 | grep inet | awk '{print $2}' | cut -d/ -f1"
+#cmd = "ip addr show eth0 | grep inet | awk '{print $2}' | cut -d/ -f1"
+ 
+ 
+def run_cmd(eth0):
+        p = Popen(cmd, shell=True, stdout=PIPE)
+        output = p.communicate()[0]
+        return output
+
+ 
+while 1:
+        #lcd.lcd_clear()
+        ipaddr = run_cmd(cmd)
+        lcd.lcd_display_string(datetime.now().strftime('%b %d  %H:%M:%S\n'),1)
+        lcd.lcd_display_string('IP %s' % ( ipaddr ),2)
+        sleep(2)
+
+
+
+
---
+++ LCD/i2C_LCD/readme.md
@@ -0,0 +1,1 @@
+Forked from: https://github.com/mmmarq/raspberry_lcd
