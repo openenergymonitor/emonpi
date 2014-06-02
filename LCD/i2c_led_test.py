08d60bf7167252b6aa59a490c0301495ddc3549f
Glyn Hudson <glyn.hudson@gmail.com>
2014-06-02 15:45:44 +0100

    remove old files

--- LCD/i2c_led_test.py
+++
@@ -1,6 +0,0 @@
-import pylcdlib
-lcd = pylcdlib.lcd(0x27,1,1)  #raspberry pi ver2, (I2C address, I2C bus 512Mb Pi=1 256Mb Pi=0, reverse code 0-2)  
-lcd.lcd_puts("emonPi",1) 
-lcd.lcd_puts("Test LCD",2)  
-
-
