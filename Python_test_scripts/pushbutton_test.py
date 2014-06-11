a2dfb54754d9c6eb94cd5c683151d66754223354
Glyn Hudson <glyn.hudson@gmail.com>
2014-06-11 21:18:10 +0000

    push button to count presses

--- Python_test_scripts/pushbutton_test.py
+++ Python_test_scripts/pushbutton_test.py
@@ -1,23 +1,36 @@
+from time import sleep
 import RPi.GPIO as GPIO
 
-GPIO.setmode(GPIO.BOARD)
+press_count =0
 
+GPIO.setmode(GPIO.BOARD)
 GPIO.setup(16, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
 
-def printFunction(channel):
 
-	print('Button 1 pressed!')
 
-	print('Note how the bouncetime affects the button press')
+def buttonPress(channel):
+	global press_count
+	print('Button press! ')
+	press_count += 1
+
+
+
+
 
 
 # Define a function to keep script running
 def loop():
     raw_input()
 
-GPIO.add_event_detect(16, GPIO.RISING, callback=printFunction, bouncetime=200)
+press_num=0
+GPIO.add_event_detect(16, GPIO.RISING, callback=buttonPress, bouncetime=200)
 
-loop()
+
+
+while 1:
+	print(press_count)
+	sleep(1)
+
 
 	
 
