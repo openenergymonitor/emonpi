69ce3ad0ae0c9a5d516b132d000333b30d5a8515
Glyn Hudson <glyn.hudson@gmail.com>
2015-02-05 19:03:50 +0000

    re write LCD script for push button

--- lcd/emonPiLCD.py
+++ lcd/emonPiLCD.py
@@ -1,37 +1,43 @@
 #!/usr/bin/env python
 
 import lcddriver
+lcd = lcddriver.lcd()
+
 from subprocess import *
 from time import sleep, strftime
 from datetime import datetime
-lcd = lcddriver.lcd()
 
+import threading
 
 import RPi.GPIO as GPIO
 
-GPIO.setmode(GPIO.BOARD)                                 #Use Pi board pin numbers as these as always consistant between revisions 
 
-GPIO.setup(16, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)    #emonPi push button Pin 16 GPIO 23
+# Use Pi board pin numbers as these as always consistant between revisions 
+GPIO.setmode(GPIO.BOARD)                                 
 
-software_rev= 0.1
-firmware_rev=0.1
+#emonPi push button Pin 16 GPIO 23
+GPIO.setup(16, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)    
 
-print 'emonPi V%s' %(software_rev)
-print 'Firmware: V%s'%(firmware_rev)
+
+
+
 print datetime.now().strftime('%b %d %H:%M')
 
+class ButtonInput():
 
-def buttonPress(channel):
-    print('Button 1 pressed!')
-GPIO.add_event_detect(16, GPIO.RISING, callback=buttonPress, bouncetime=200)
+    def __init__(self):
+        GPIO.add_event_detect(16, GPIO.RISING, callback=self.buttonPress, bouncetime=500) 
+        self.press_num = 0
 
-def lcd_startup(software_rev, firmware_rev):
-    lcd.lcd_display_string('emonPi V%s' %(software_rev), 1)
-    lcd.lcd_display_string('Firmware: V%s'%(firmware_rev) ,2)  
-    sleep(2)
-    return()
-lcd_startup(software_rev, firmware_rev)
- 
+    def buttonPress(self,channel):
+        print('Button 1 pressed!') 
+        self.press_num = self.press_num + 1 
+
+buttoninput = ButtonInput()
+#Setup callback function buttonpress to appen on press of push button    
+
+
+
 # Return local IP address for eth0 or wlan0
 def local_IP():
 	wlan0 = "ip addr show wlan0 | grep inet | awk '{print $2}' | cut -d/ -f1 | head -n1"
@@ -44,11 +50,11 @@ def local_IP():
 		IP = p.communicate()[0].rstrip('\n')
 		network = "wlan0"
 	return {IP , network}
-IP, network = local_IP()
-print IP
-print network
+#IP, network = local_IP()
+#print IP
+#print network
 
-#Test to see if we have working internet connection 
+#Test to see if we have working internet connection to emoncms.org 
 import socket
 REMOTE_SERVER = "www.google.com"
 def is_connected():
@@ -63,67 +69,56 @@ def is_connected():
   except:
      pass
   return False
-print 'Internet connected? %s' %(is_connected())
+#print 'Internet connected? %s' %(is_connected())
 
-sleep(5)
+#update lcd 
+def updatelcd():
+    threading.Timer(3, updatelcd).start()
+    lcd.lcd_clear();
+
  
 while 1:
-        lcd.lcd_clear()
-        IP, network = local_IP()
 
+    ##updatelcd();
+
+    if buttoninput.press_num == 0:   
+        print "screen 0" 
+        print buttoninput.press_num
+        IP, network = local_IP()
         if IP == "":
             lcd.lcd_display_string('Awaiting Network',1) 
             lcd.lcd_display_string('Connection......',2)
             
         if IP != "":
-            lcd.lcd_display_string('%s connected' % (network),1)
-            lcd.lcd_display_string('IP: %s' % ( IP ),2)
+            lcd.lcd_display_string('%s connected' % (IP),1)
+            lcd.lcd_display_string('IP: %s' % ( network ),2)
 
-        sleep(5)
-        lcd.lcd_clear()
+    elif buttoninput.press_num == 1:          
+        print "screen 2"
+        print buttoninput.press_num
+        lcd.lcd_display_string('Checking WAN    ',1) 
+        lcd.lcd_display_string('Connection......',2)
+        #if is_connected() == True:
+        #    lcd.lcd_display_string('Internet',1)
+        #    lcd.lcd_display_string('Connected',2)
+        #else:
+        #    lcd.lcd_display_string('Internet',1)
+        #    lcd.lcd_display_string('Connection FAIL',2)
 
-        if is_connected() == True:
-        	lcd.lcd_display_string('Internet',1)
-        	lcd.lcd_display_string('Connected',2)
-        else:
-            lcd.lcd_display_string('Internet',1)
-            lcd.lcd_display_string('Connection FAIL',2)
-
-        sleep(5)
-
-        lcd.lcd_clear()
+    elif buttoninput.press_num == 2: 
+        print "screen 2"
+        print buttoninput.press_num       
         lcd.lcd_display_string(datetime.now().strftime('%b %d %H:%M'),1)
         lcd.lcd_display_string(datetime.now().strftime('Uptime: 123 days'),2)
-        sleep(5)
 
-       
-        lcd.lcd_clear()
-        lcd.lcd_display_string(datetime.now().strftime('Found AC Voltage'),1)
-        lcd.lcd_display_string(datetime.now().strftime('240.2V RMS'),2)
-        sleep(5)
+    else:
+        print "reset"
+        print buttoninput.press_num
+        buttoninput.press_num = 0
 
-        lcd.lcd_clear()
-        lcd.lcd_display_string(datetime.now().strftime('Real Power '),1)
-        lcd.lcd_display_string(datetime.now().strftime('Sampling Active'),2)
-        sleep(5)
-  
-        lcd.lcd_clear()
-        lcd.lcd_display_string(datetime.now().strftime('CT1 Now: 25000W'),1)
-        lcd.lcd_display_string(datetime.now().strftime('CT2 Now: 120W'),2)
-        sleep(5)
+#
 
-        lcd.lcd_clear()
-        lcd.lcd_display_string(datetime.now().strftime('CT1:005451.2 KWh'),1)
-        lcd.lcd_display_string(datetime.now().strftime('CT2:000065.2 KWh'),2)
-        sleep(5)
-
-        lcd.lcd_clear()
-        lcd.lcd_display_string(datetime.now().strftime('Found 3 DS18B20'),1)
-        lcd.lcd_display_string(datetime.now().strftime('20.2 18.3 16.5'),2)
-        sleep(2)
-        lcd.lcd_display_string(datetime.now().strftime('Temperatures:  '),1)
-        sleep(5)
-
+            
 GPIO.cleanup()
         
 
