29d7a3975bf14599a6f7bef41cdfe3185a23edff
Glyn Hudson <glyn.hudson@gmail.com>
2015-03-17 12:00:41 +0000

    LCD script for pi, tasks carried out in background thread accessed via redis in foreground thread

--- lcd/emonPiLCD.py
+++ lcd/emonPiLCD.py
@@ -3,24 +3,84 @@
 import lcddriver 
 lcd = lcddriver.lcd()
 from subprocess import *
-from time import sleep, strftime
+import time
 from datetime import datetime
 from datetime import timedelta
 from uptime import uptime
 import threading
 import sys
 import RPi.GPIO as GPIO
+import signal
+import redis
+import re
 
+r = redis.Redis(host='localhost', port=6379, db=0)
+background = False
 
-# Use Pi board pin numbers as these as always consistent between revisions 
-GPIO.setmode(GPIO.BOARD)                                 
+class Background(threading.Thread):
 
-#emonPi LCD push button Pin 16 GPIO 23
-GPIO.setup(16, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)    
-#emonPi Shutdown button, Pin 11 GPIO 17
-GPIO.setup(11, GPIO.IN)
-
-print "OpenEnergyMonitor - emonPi LCD / Shutdown Python Script"
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.stop = False
+        
+    def run(self):
+        last1s = time.time() - 2.0
+        last5s = time.time() - 6.0
+        # Loop until we stop is false (our exit signal)
+        while not self.stop:
+            now = time.time()
+            
+            # ----------------------------------------------------------
+            # UPDATE EVERY 1's
+            # ----------------------------------------------------------
+            if (now-last1s)>=1.0:
+                last1s = now
+                # Get uptime
+                with open('/proc/uptime', 'r') as f:
+                    seconds = float(f.readline().split()[0])
+                    array = str(timedelta(seconds = seconds)).split('.')
+                    string = array[0]
+                    r.set("uptime",seconds)
+                    
+            # ----------------------------------------------------------
+            # UPDATE EVERY 5's
+            # ----------------------------------------------------------
+            if (now-last5s)>=5.0:
+                last5s = now
+                
+                # Ip address's
+                wlan0 = "ip addr show wlan0 | grep inet | awk '{print $2}' | cut -d/ -f1 | head -n1"
+                eth0 = "ip addr show eth0 | grep inet | awk '{print $2}' | cut -d/ -f1 | head -n1"
+                p = Popen(eth0, shell=True, stdout=PIPE)
+                IP = p.communicate()[0]
+                IP = IP[:-1] #remove blank space at end of IP
+                network = "eth"
+                if IP == "":
+                    p = Popen(wlan0, shell=True, stdout=PIPE)
+                    IP = p.communicate()[0].rstrip('\n')
+                    network = "wlan"
+                r.set("ipaddress",IP)
+                r.set("network",network)
+                
+                if network=="wlan":
+                    # wlan link status
+                    p = Popen("/sbin/iwconfig wlan0", shell=True, stdout=PIPE)
+                    iwconfig = p.communicate()[0]
+                    signallevel = re.findall('(?<=Signal level=)\w+',iwconfig)[0]
+                    linklevel = re.findall('(?<=Link Quality=)\w+',iwconfig)[0]
+                    noiselevel = re.findall('(?<=Noise level=)\w+',iwconfig)[0]
+                    r.set("wlan:signallevel",signallevel)
+                    r.set("wlan:linklevel",linklevel)
+                    r.set("wlan:noiselevel",noiselevel)
+                
+            # this loop runs a bit faster so that ctrl-c exits are fast
+            time.sleep(0.1)
+            
+def sigint_handler(signal, frame):
+    """Catch SIGINT (Ctrl+C)."""
+    print "SIGINT received."
+    background.stop = True;
+    sys.exit(0)
 
 def shutdown():
     while (GPIO.input(11) == 1):
@@ -29,19 +89,19 @@ def shutdown():
         lcd.lcd_display_string( string_lenth(lcd_string1, 16),1)
         lcd.lcd_display_string( string_lenth(lcd_string2, 16),2)
         print lcd_string1
-        sleep(1)
+        time.sleep(1)
         for x in range(4, 0, -1):
             lcd_string2 += "%d.." % (x)
             lcd.lcd_display_string( string_lenth(lcd_string2, 16),2) 
             print lcd_string2
-            sleep(1)
+            time.sleep(1)
             
             if (GPIO.input(11) == 0):
                 return
         lcd_string2="SHUTDOWN NOW!"
         lcd.lcd_display_string( string_lenth(lcd_string1, 16),1)
         lcd.lcd_display_string( string_lenth(lcd_string2, 16),2) 
-        sleep(2)
+        time.sleep(2)
         lcd.lcd_clear()
         lcd.lcd_display_string( string_lenth("Power", 16),1)
         lcd.lcd_display_string( string_lenth("Off", 16),2)
@@ -49,59 +109,20 @@ def shutdown():
         call('halt', shell=False)
         sys.exit() #end script 
 
-
 class ButtonInput():
     def __init__(self):
-        GPIO.add_event_detect(16, GPIO.RISING, callback=self.buttonPress, bouncetime=200) 
+        GPIO.add_event_detect(16, GPIO.RISING, callback=self.buttonPress, bouncetime=1000) 
         self.press_num = 0
+        self.pressed = False
     def buttonPress(self,channel):
         print self.press_num
-        self.press_num = self.press_num + 1 
-        print uptime_days
-buttoninput = ButtonInput()
-#Setup callback function buttonpress to appen on press of push button    
-
+        self.press_num = self.press_num + 1
+        if self.press_num>3: self.press_num = 0
+        self.pressed = True
+                    
 def get_uptime():
-    with open('/proc/uptime', 'r') as f:
-        seconds = float(f.readline().split()[0])
-        array = str(timedelta(seconds = seconds)).split('.')
-        string = array[0]
+
     return string
-
-
-# Return local IP address for eth0 or wlan0
-def local_IP():
-	wlan0 = "ip addr show wlan0 | grep inet | awk '{print $2}' | cut -d/ -f1 | head -n1"
-	eth0 = "ip addr show eth0 | grep inet | awk '{print $2}' | cut -d/ -f1 | head -n1"
-	p = Popen(eth0, shell=True, stdout=PIPE)
-	IP = p.communicate()[0]
-	IP = IP[:-1] 												#remove blank space at end of IP
-	network = "eth0"
-	if IP == "":
-		p = Popen(wlan0, shell=True, stdout=PIPE)
-		IP = p.communicate()[0].rstrip('\n')
-		network = "wlan0"
-	return {IP , network}
-#IP, network = local_IP()
-#print IP
-#print network
-
-#Test to see if we have working internet connection to emoncms.org 
-import socket
-REMOTE_SERVER = "www.google.com"
-def is_connected():
-  try:
-    # see if we can resolve the host name -- tells us if there is
-    # a DNS listening
-    host = socket.gethostbyname(REMOTE_SERVER)
-    # connect to the host -- tells us if the host is actually
-    # reachable
-    s = socket.create_connection((host, 80), 2)
-    return True
-  except:
-     pass
-  return False
-#print 'Internet connected? %s' %(is_connected())
 
 def string_lenth(string, length):
 	# Add blank characters to end of string to make up to length long
@@ -111,57 +132,72 @@ def string_lenth(string, length):
 
 # write to I2C LCD 
 def updatelcd():
-    lcd.lcd_display_string( string_lenth(lcd_string1, 16),1) # line 1- make sure string is 16 characters long to fill LED 
+    # line 1- make sure string is 16 characters long to fill LED 
+    lcd.lcd_display_string( string_lenth(lcd_string1, 16),1)
+    print lcd_string1
     lcd.lcd_display_string( string_lenth(lcd_string2, 16),2) # line 2
+    print lcd_string2
 
+# Use Pi board pin numbers as these as always consistent between revisions 
+GPIO.setmode(GPIO.BOARD)                                 
+#emonPi LCD push button Pin 16 GPIO 23
+GPIO.setup(16, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)    
+#emonPi Shutdown button, Pin 11 GPIO 17
+GPIO.setup(11, GPIO.IN)
 
-def calc_uptime_days():
-	uptime_days = uptime() / 86400
-	threading.Timer(60, calc_uptime_days).start()
-	return(uptime_days)
-	
-calc_uptime_days()
+print "OpenEnergyMonitor - emonPi LCD / Shutdown Python Script"
 
+lcd_string1 = ""
+lcd_string2 = ""
+
+signal.signal(signal.SIGINT, sigint_handler)
+
+background = Background()
+background.start()
+buttoninput = ButtonInput()
+
+last1s = time.time() - 1.0
 while 1:
-
-	uptime_days=calc_uptime_days()
-
-	if buttoninput.press_num == 0:
-		IP, network = local_IP()
-        if IP == "":
-            lcd_string1 = 'Awaiting Network'
-            lcd_string2 = 'Connection......'
-
-        if IP != "":
-        	lcd_string1 = '%s connected' % (network)
-        	lcd_string2 = 'IP: %s' % (IP)
-
-    #elif buttoninput.press_num == 1:          
-    #    lcd_string1 = 'Checking WAN    '
-    #    lcd_string2 = 'Connection......'
-        #if is_connected() == True:
-        #    lcd_string1 = 'Internet'
-        #    lcd_string2 = 'Connected'
-        #else:
-        #    lcd_string1 = 'Internet'
-        #    lcd_string2 = 'Connection FAIL'
-
-	elif buttoninput.press_num == 1:
-		lcd_string1 = datetime.now().strftime('%b %d %H:%M')
-		lcd_string2 =  'Uptime %.2f days' % (uptime_days)
+    now = time.time()
     
-	elif buttoninput.press_num == 2: 
-		lcd_string1 = 'Power: 563W'
-		lcd_string2 = 'Today: 1.3KW'
-	else:
-		buttoninput.press_num = 0
+    # ----------------------------------------------------------
+    # UPDATE EVERY 1's
+    # ----------------------------------------------------------
+    if (now-last1s)>=1.0 or buttoninput.pressed:
+        last1s = now
+        buttoninput.pressed = False
+        
+        if buttoninput.press_num==0:
+            IP = r.get("ipaddress")
+            if IP == "":
+                lcd_string1 = 'Awaiting Network'
+                lcd_string2 = 'Connection......'
+            else:
+                lcd_string1 = str(r.get("network"))+"    SIG:"+str(r.get("wlan:signallevel"))+"%"
+                lcd_string2 = "IP "+IP
+            
+        elif buttoninput.press_num==1:
+            lcd_string1 = "Link|Sig|Noise"
+            if r.get("network")=="wlan":
+                lcd_string2 = r.get("wlan:linklevel")+"% "+r.get("wlan:signallevel")+"% "+r.get("wlan:noiselevel")+"%"
+            else:
+                lcd_string2 = ""
 
-# only update LCD if shutdown button is not pressed
-	if (GPIO.input(11) == 0): 
-		updatelcd()
-	else:
-		shutdown()
-
+        elif buttoninput.press_num==2:
+		    lcd_string1 = datetime.now().strftime('%b %d %H:%M')
+		    lcd_string2 =  'Uptime %.2f days' % (float(r.get("uptime"))/86400)
+		    
+        elif buttoninput.press_num==3: 
+            lcd_string1 = 'Power: 563W'
+            lcd_string2 = 'Today: 1.3KW'
+        
+        if (GPIO.input(11) == 0):
+            updatelcd()
+        else:
+            shutdown()
+    
+    time.sleep(0.1)
+    
 GPIO.cleanup()
         
 
