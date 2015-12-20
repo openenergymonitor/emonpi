dd17a9f890f221f88c1c1eaefd68224c68efbfbe
Glyn Hudson <glyn.hudson@gmail.com>
2015-12-20 21:48:56 +0000

    fix update error message due to stopping emonPi LCD twice

--- lcd/emonPiLCD_update.py
+++ lcd/emonPiLCD_update.py
@@ -3,11 +3,26 @@
 import lcddriver
 import time
 import sys
+import subprocess
 
+# ------------------------------------------------------------------------------------
+# Check to see if LCD is connected if not then stop here
+# ------------------------------------------------------------------------------------
+
+lcd_status = subprocess.check_output(["/home/pi/emonpi/lcd/./emonPiLCD_detect.sh", "27"])
+
+if lcd_status.rstrip() == 'False':
+    print "I2C LCD NOT DETECTED"
+    sys.exit(1)
+    
 lcd = lcddriver.lcd()
 
+# ------------------------------------------------------------------------------------
+# Display update in progress update
+# ------------------------------------------------------------------------------------
+
 lcd.lcd_display_string("Updating........", 1)
-lcd.lcd_display_string("DO NOT UNPLUG!  ",2)  
+lcd.lcd_display_string("DO NOT UNPLUG!  ",2)
 time.sleep(1)
 sys.exit()
 
