b4991f20930507be089c416554712a931447c644
basazababo@gmail.com <basazababo@gmail.com>
2015-12-22 13:34:00 +0000

    updated install to work on bbb

--- lcd/lcddriver.py
+++ lcd/lcddriver.py
@@ -3,7 +3,7 @@ from time import *
 
 # LCD Address
 ADDRESS = 0x27
-
+PORT=2
 # commands
 LCD_CLEARDISPLAY = 0x01
 LCD_RETURNHOME = 0x02
@@ -53,7 +53,7 @@ Rs = 0b00000001 # Register select bit
 class lcd:
    #initializes objects and lcd
    def __init__(self):
-      self.lcd_device = i2c_lib.i2c_device(ADDRESS)
+      self.lcd_device = i2c_lib.i2c_device(ADDRESS,PORT)
 
       self.lcd_write(0x03)
       self.lcd_write(0x03)
