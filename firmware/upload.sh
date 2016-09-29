76320a540b6fb929f65506d7e18a7b18959363ff
Glyn <glyn.hudson@gmail.com>
2016-09-29 11:37:01 +0000

    fix emonhub start typo

--- firmware/upload.sh
+++ firmware/upload.sh
@@ -3,16 +3,19 @@
 # Compile and upload direclty to the emonPi from the emonPi using Platformio
 
 # make FS RW
+echo "make file-system read-write"
 rpi-rw
 
 # stop openhub to free up serial port /dev/ttyAMA0
-
+echo "stop emonhub"
 sudo service emonhub stop
 
-# Compile & upload using platformio
+echo "Compile & upload using platformio"
 sudo pio run -t upload
 
-sudop service emonhub start
+echo "start emonhub"
+sudo service emonhub start
 
+echo "put file system back to read-only"
 # make FS readonly 
 rpi-ro
