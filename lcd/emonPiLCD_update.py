3d0967506a8e2033b4b7ae19e607671de95ddfa2
Glyn Hudson <glyn.hudson@gmail.com>
2016-12-20 11:09:29 +0000

    lcd updating display script now supports i2c lcd addr scan

--- lcd/emonPiLCD_update.py
+++ lcd/emonPiLCD_update.py
@@ -8,14 +8,21 @@ import subprocess
 # ------------------------------------------------------------------------------------
 # Check to see if LCD is connected if not then stop here
 # ------------------------------------------------------------------------------------
+lcd_i2c = ['27', '3f']
+current_lcd_i2c = ''
 
-lcd_status = subprocess.check_output(["/home/pi/emonpi/lcd/./emonPiLCD_detect.sh", "27"])
+for i2c_address in lcd_i2c:
+  lcd_status = subprocess.check_output(["/home/pi/emonpi/lcd/emonPiLCD_detect.sh", "%s" % i2c_address])
+  if lcd_status.rstrip() == 'True':
+    print "I2C LCD DETECTED Ox%s" % i2c_address
+    current_lcd_i2c = "0x%s" % i2c_address
+    break
 
 if lcd_status.rstrip() == 'False':
-    print "I2C LCD NOT DETECTED"
-    sys.exit(1)
-    
-lcd = lcddriver.lcd()
+  print ("I2C LCD NOT DETECTED on either 0x" + str(lcd_i2c) + " ...exiting LCD script")
+  sys.exit(1)
+  
+lcd = lcddriver.lcd(int(current_lcd_i2c, 16))
 
 # ------------------------------------------------------------------------------------
 # Display update in progress update
@@ -25,7 +32,3 @@ lcd.lcd_display_string("Updating........", 1)
 lcd.lcd_display_string("DO NOT UNPLUG!  ",2)
 time.sleep(1)
 sys.exit()
-
-
-
-
