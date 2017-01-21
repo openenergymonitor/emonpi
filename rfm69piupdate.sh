5861c719735fd0e9bf762d123903ff6382286e7c
Glyn Hudson <glyn.hudson@gmail.com>
2017-01-21 10:34:02 +0000

    add more checking to rfm69pi update and stop emonhub

--- rfm69piupdate.sh
+++ rfm69piupdate.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-
-echo "getting latest RFM69Pi release info from github"
+echo "RFM69Pi Update Started"
+echo  date
+echo "Getting latest RFM69Pi release info from github"
 download_url="$(curl -s https://api.github.com/repos/openenergymonitor/RFM2Pi/releases | grep browser_download_url | head -n 1 | cut -d '"' -f 4)"
 version="$(curl -s https://api.github.com/repos/openenergymonitor/RFM2Pi/releases | grep tag_name | head -n 1 |  cut -d '"' -f 4)"
 echo "Latest RFM69Pi firmware: V"$version
@@ -15,14 +16,18 @@ fi
 
 wget -q $download_url -O /home/pi/data/firmware/rfm69pi-$version.hex
 
-echo
-echo "================================="
-echo "RFM69Pi update started"
-echo "================================="
-echo
-echo "Flashing RFM69Pi with V" $version
-echo
-
-
-avrdude -v -c arduino -p ATMEGA328P -P /dev/ttyAMA0 -b 38400 -U flash:w:/home/pi/data/firmware/rfm69pi-$version.hex
-
+if [ -f /home/pi/data/firmware/rfm69pi-$version.hex ]; then
+  sudo service emonhub stop
+  echo
+  echo "================================="
+  echo "RFM69Pi update started"
+  echo "================================="
+  echo
+  echo "Flashing RFM69Pi with V" $version
+  echo
+  avrdude -v -c arduino -p ATMEGA328P -P /dev/ttyAMA0 -b 38400 -U flash:w:/home/pi/data/firmware/rfm69pi-$version.hex
+  sudo service emonhub start
+  echo "Flashing RFM69Pi with V" $version " done"
+else
+ echo "Firmware download failed...check network connection"
+fi
