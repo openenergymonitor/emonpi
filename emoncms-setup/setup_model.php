cdf64fb7e1faa71e75fe19946f58f1d081d00780
Trystan Lea <trystan.lea@gmail.com>
2017-06-15 22:37:27 +0000

    deal with case where wifi is configured in client mode in wpa_supplicant but flag in setup db is not yet set

--- emoncms-setup/setup_model.php
+++ emoncms-setup/setup_model.php
@@ -19,18 +19,14 @@ class Setup
         if ($result && $row = $result->fetch_object()) {
             $wifi = $row->wifi;
         }
-        
-        // Special case, if setup flag in database is wifi client but wpa_supplicant is blank then something went wrong
-        // or user cleared wpa_supplicant deliberatly in order to move emonpi to another network.
-        // If this is the case then we flag up as unconfigured
-        if ($wifi=="client") {
-            $wpa_supplicant = file_get_contents("/home/pi/data/wpa_supplicant.conf");
-            // for ($i=0; $i<strlen($wpa_supplicant); $i++) print $wpa_supplicant[$i]." ".ord($wpa_supplicant[$i])."<br>";
-            if ($wpa_supplicant=="ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev\nupdate_config=1\n") {
-                $wifi = "unconfigured";
-            }
+
+        $wpa_supplicant = file_get_contents("/home/pi/data/wpa_supplicant.conf");
+        if ($wpa_supplicant=="ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev\nupdate_config=1\n") {
+            if ($wifi=="client") $wifi = "unconfigured";
+        } else {
+            if ($wifi=="unconfigured") $wifi = "client";
         }
-        
+
         return $wifi;
     }
 }
