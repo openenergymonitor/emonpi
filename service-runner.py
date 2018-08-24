b2f403a9cce56045c95c5acba3e8814877a81bfa
greeebs <rjsparkes@gmail.com>
2018-08-24 14:35:18 +0000

    Fixed copy/paste spacing issue while trying to tidy up commits.

--- service-runner.py
+++ service-runner.py
@@ -1,20 +1,26 @@
 #!/usr/bin/python
- import sys
+
+import sys
 import redis
 import subprocess
 import time
 import signal
- def handle_sigterm(sig, frame):
+
+def handle_sigterm(sig, frame):
   print("Got Termination signal, exiting")
   sys.exit(0)
- ## Used to update log viewer window in Emoncms admin
+
+## Used to update log viewer window in Emoncms admin
 # Used in conjunction with: service-runner-update.sh and Emoncms admin module
- # Setup the signal handler to gracefully exit
+
+# Setup the signal handler to gracefully exit
 signal.signal(signal.SIGTERM, handle_sigterm)
 signal.signal(signal.SIGINT, handle_sigterm)
- print("Starting service-runner")
+
+print("Starting service-runner")
 sys.stdout.flush()
- server = redis.Redis()
+
+server = redis.Redis()
 while True:
   try:
     if server.exists('service-runner'):
