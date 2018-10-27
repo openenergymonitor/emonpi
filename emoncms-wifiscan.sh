bf9d3dc90fd999afecd00be4d35f7c3a4cd1dce7
Glyn Hudson <glyn.hudson@gmail.com>
2018-10-27 22:34:56 +0000

    add shebang to fix emoncms/emoncms/#1089

--- emoncms-wifiscan.sh
+++ emoncms-wifiscan.sh
@@ -1,1 +1,4 @@
-sudo php /home/pi/emonpi/emoncms_wifiscan.php
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+sudo php $DIR/emoncms_wifiscan.php
+#sudo php /home/pi/emonpi/emoncms_wifiscan.php
