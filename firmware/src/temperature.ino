0e66fb5f616e1aade669742bbdca98695a9cafaa
GIT Admin <alandpearson@gmail.com>
2018-11-25 19:05:58 +0000

    Add support for Dallas DS2438 temperature readings The DS2438 is commonly used in a lot of ibuttonlink.com sensors which are very popular https://www.ibuttonlink.com/collections/1-wire-temperature-sensors
    These plug straight into the emonpi and are very useful.
    
    The DS2438 also has other capabilites for measuring DC Voltage and
    current which are not added in this commit. Maybe someday..

--- firmware/src/temperature.ino
+++ firmware/src/temperature.ino
@@ -2,29 +2,68 @@
 byte check_for_DS18B20()
 {
   sensors.begin();
-  numSensors=(sensors.getDeviceCount());
-  
-  byte j=0;                                        // search for one wire devices and
-                                                   // copy to device address arrays.
-  while ( (j < numSensors) && (sensors.getAddress(allAddress[j], j)) )  j++;
-  for(byte j=0;j<numSensors;j++) sensors.setResolution(allAddress[j], TEMPERATURE_PRECISION);      // and set the a to d conversion resolution of each.
-  
-  if (numSensors==0) DS18B20_STATUS=0;
-    else DS18B20_STATUS=1;
+  numSensors = (sensors.getDeviceCount());
 
-  if (numSensors>MaxOnewire) numSensors=MaxOnewire;     //If more sensors are detected than allowed only read from max allowed number
-    
- return numSensors;
+  byte j = 0;                                      // search for one wire devices and
+  // copy to device address arrays.
+  while ( (j < numSensors) && (sensors.getAddress(allAddress[j], j)) )  j++;
+  for (byte j = 0; j < numSensors; j++) sensors.setResolution(allAddress[j], TEMPERATURE_PRECISION); // and set the a to d conversion resolution of each.
+
+  if (numSensors == 0) DS18B20_STATUS = 0;
+  else DS18B20_STATUS = 1;
+
+  if (numSensors > MaxOnewire) numSensors = MaxOnewire; //If more sensors are detected than allowed only read from max allowed number
+
+  return numSensors;
 }
 
-int get_temperature(byte sensor)
-{
-  {
-    float temp=(sensors.getTempC(allAddress[sensor]));
+int get_temperature(byte sensor) {
 
-    if ((temp<125.0) && (temp>-55.0))
-      return (temp*10);            //if reading is within range for the sensor convert float to int ready to send via RF
+  float temp = 300 ;
+  /*
+    if (DEBUG) {
+    Serial.print("Sensor address = " );
+    for(int x=8; x>0 ; x--){
+      Serial.print(allAddress[sensor][x], HEX) ;
+      Serial.print(" ") ;
+    }
+    Serial.println();
+    }
+  */
+  if (allAddress[sensor][0] == 0x26) {
+    DS2438 ds2438(&oneWire, allAddress[sensor]);
+    ds2438.begin() ;
+    ds2438.update() ;
+    if (ds2438.isError()) {
+      //Serial.println("Error reading from DS2438 device");
+      temp = -55 ;
+    } else {
+      temp = ds2438.getTemperature() ;
+      /*
+      if (DEBUG) {
+        Serial.print("DS2438 Temperature = ");
+        Serial.print(temp, 1);
+        Serial.println() ;
+      }
+      */
+    }
+  } else if (allAddress[sensor][0] == 0x28) {
+
+    temp = (sensors.getTempC(allAddress[sensor]));
+    /*
+    if (DEBUG) {
+      Serial.print("DS18x20 Temperature = ");
+      Serial.print(temp, 1);
+      Serial.println() ;
+    }
+    */
   }
-  return -55;  // out of range return minimum
+
+  if ((temp < 125.0) && (temp > -55.0)) {
+    return (temp * 10);         //if reading is within range for the sensor convert float to int ready to send via RF
+  } else {
+    return -55;  // out of range return minimum
+  }
+
 }
 
