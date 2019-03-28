16221062d48423a46050ea06dab8dd58d4e87825
TrystanLea <glyn.hudson@openenergymonitor.org>
2019-03-28 15:46:19 +0000

    check version before emonpi git pull

--- service-runner-update.sh
+++ service-runner-update.sh
@@ -16,13 +16,39 @@ fi
 # Clear log update file
 cat /dev/null > $usrdir/data/emonpiupdate.log
 
-echo "Starting update via service-runner-update.sh (v1.1.1) >"
-
-# -----------------------------------------------------------------
+echo "Starting update via service-runner-update.sh (v2.0) >"
 
 # make file system read-write
 if [ -f /usr/bin/rpi-rw ]; then
   rpi-rw
+fi
+
+# -----------------------------------------------------------------
+
+# Check emonSD version
+image_version=$(ls /boot | grep emonSD)
+
+if [ "$image_version" = "" ]; then
+    echo "- Could not find emonSD version file"
+    emonSD_pi_env=0
+else 
+    echo "- emonSD version: $image_version"
+    valid=0
+    
+    save_to_update=$(curl -s https://raw.githubusercontent.com/openenergymonitor/emonpi/master/safe-update)
+    while read -r image_name; do
+        if [ "$image_version" == "$image_name" ]; then
+            echo "emonSD base image check passed...continue update"
+            valid=1
+        fi
+    done <<< "$save_to_update"
+
+    if [ $valid == 0 ]; then
+        echo "ERROR: emonSD base image old or undefined...update will not continue"
+        echo "See latest verson: https://github.com/openenergymonitor/emonpi/wiki/emonSD-pre-built-SD-card-Download-&-Change-Log"
+        echo "Stopping update"
+        exit 0
+    fi
 fi
 
 # -----------------------------------------------------------------
