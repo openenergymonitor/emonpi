7a21a2ef7386ddd9bffd040fbb86e5bebb29f934
Trystan Lea <trystan.lea@gmail.com>
2019-04-02 22:07:34 +0100

    updated install script for emonPiLCD

--- install/emonpilcd.sh
+++ install/emonpilcd.sh
@@ -7,3 +7,22 @@ sudo pip install xmltodict
 sudo sed -i "s/^#dtparam=i2c_arm=on/dtparam=i2c_arm=on/" /boot/config.txt
 # Append line i2c-dev to /etc/modules
 sudo sed -i -n '/i2c-dev/!p;$a i2c-dev' /etc/modules
+
+# emonPiLCD folder is symlinked to /usr/share
+# service expects python script to be at this location
+sudo ln -s $usrdir/emonpi/lcd/ /usr/share/emonPiLCD
+
+# emonPiLCD Logger
+sudo mkdir /var/log/emonpilcd
+# Permissions?
+touch /var/log/emonpilcd/emonpilcd.log
+
+# EmonBase / EmonPi flags are stored in /home/pi/data
+cd
+mkdir data
+
+# Install and start emonPiLCD service
+sudo ln -s /usr/share/emonPiLCD/emonPiLCD.service /lib/systemd/system
+sudo systemctl daemon-reload
+sudo systemctl enable emonPiLCD.service
+sudo systemctl start emonPiLCD.service
