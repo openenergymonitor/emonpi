6a7c4d9683fc90ce15f94167b80e2097c0b6afb1
Trystan Lea <trystan.lea@googlemail.com>
2019-04-05 21:41:19 +0100

    Update emonhub.sh

--- install/emonhub.sh
+++ install/emonhub.sh
@@ -4,6 +4,7 @@ source config.ini
 echo "-------------------------------------------------------------"
 echo "emonHub install"
 echo "-------------------------------------------------------------"
+cd $usrdir
 
 if [ ! -d $usrdir/emonhub ]; then
     git clone https://github.com/openenergymonitor/emonhub.git
