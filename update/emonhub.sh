b74caff8fd005c54bfeec653c5895674d9bf9d04
Trystan Lea <trystan.lea@googlemail.com>
2019-05-21 11:17:31 +0100

    Update emonhub.sh

--- update/emonhub.sh
+++ update/emonhub.sh
@@ -13,7 +13,12 @@ if [ -d $usrdir/emonhub ]; then
     git branch
     git status
     git pull
-
+    
+    if [ ! -d /var/log/emonhub ]; then
+        sudo mkdir /var/log/emonhub
+        sudo chown emonhub:emonhub /var/log/emonhub
+    fi
+    
     service="emonhub"
     servicepath="$usrdir/emonhub/service/emonhub.service"
     $usrdir/emonpi/update/install_emoncms_service.sh $servicepath $service
