42442136d7195b04194372942cbe1e065df816ea
Trystan Lea <trystan.lea@gmail.com>
2019-09-26 15:32:57 +0000

    settings_object support

--- update/emoncmsdbupdate.php
+++ update/emoncmsdbupdate.php
@@ -8,7 +8,13 @@ define('EMONCMS_EXEC', 1);
 chdir("/var/www/emoncms");
 require "process_settings.php";
 require "core.php";
-$mysqli = @new mysqli($server,$username,$password,$database);
+$mysqli = @new mysqli(
+    $settings["sql"]["server"],
+    $settings["sql"]["username"],
+    $settings["sql"]["password"],
+    $settings["sql"]["database"],
+    $settings["sql"]["port"]
+);
 
 require_once "Lib/dbschemasetup.php";
 print json_encode(db_schema_setup($mysqli,load_db_schema(),$applychanges))."\n";
