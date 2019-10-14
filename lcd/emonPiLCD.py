a79a3939863fc42f6f099312c619bedea9cc50ef
Bruce Duncan <bwduncan@gmail.com>
2019-10-14 22:01:46 +0100

    Fix whitespace, spelling.

--- lcd/emonPiLCD.py
+++ lcd/emonPiLCD.py
@@ -31,24 +31,24 @@ version = '3.0.1'
 # ------------------------------------------------------------------------------------
 
 config = ConfigParser.ConfigParser()
-config.read(path+'/emonPiLCD.cfg') 
+config.read(path + '/emonPiLCD.cfg')
 
 # ------------------------------------------------------------------------------------
 # MQTT Settings
 # ------------------------------------------------------------------------------------
-mqtt_user = config.get('mqtt','mqtt_user')
-mqtt_passwd = config.get('mqtt','mqtt_passwd')
-mqtt_host = config.get('mqtt','mqtt_host')
-mqtt_port = config.getint('mqtt','mqtt_port')
-mqtt_emonpi_topic = config.get('mqtt','mqtt_emonpi_topic') 
-mqtt_feed1_topic = config.get('mqtt','mqtt_feed1_topic')
-mqtt_feed2_topic = config.get('mqtt','mqtt_feed2_topic')
+mqtt_user = config.get('mqtt', 'mqtt_user')
+mqtt_passwd = config.get('mqtt', 'mqtt_passwd')
+mqtt_host = config.get('mqtt', 'mqtt_host')
+mqtt_port = config.getint('mqtt', 'mqtt_port')
+mqtt_emonpi_topic = config.get('mqtt', 'mqtt_emonpi_topic')
+mqtt_feed1_topic = config.get('mqtt', 'mqtt_feed1_topic')
+mqtt_feed2_topic = config.get('mqtt', 'mqtt_feed2_topic')
 
 # ------------------------------------------------------------------------------------
 # Redis Settings
 # ------------------------------------------------------------------------------------
-redis_host = config.get('redis', 'redis_host') 
-redis_port = config.get('redis', 'redis_port') 
+redis_host = config.get('redis', 'redis_host')
+redis_port = config.get('redis', 'redis_port')
 r = redis.Redis(host=redis_host, port=redis_port, db=0)
 
 # ------------------------------------------------------------------------------------
@@ -56,21 +56,21 @@ r = redis.Redis(host=redis_host, port=redis_port, db=0)
 # ------------------------------------------------------------------------------------
 
 # LCD backlight timeout in seconds 0: always on, 300: off after 5 min
-backlight_timeout = config.getint('general','backlight_timeout') 
-default_page = config.getint('general','default_page') 
+backlight_timeout = config.getint('general', 'backlight_timeout')
+default_page = config.getint('general', 'default_page')
 
 #Names to be displayed on power reading page
-feed1_name = config.get('general','feed1_name')
-feed2_name = config.get('general','feed2_name')
-feed1_unit = config.get('general','feed1_unit')
-feed2_unit = config.get('general','feed2_unit')
+feed1_name = config.get('general', 'feed1_name')
+feed2_name = config.get('general', 'feed2_name')
+feed1_unit = config.get('general', 'feed1_unit')
+feed2_unit = config.get('general', 'feed2_unit')
 
 #How often the LCD is updated when a button is not pressed
-lcd_update_sec = config.getint('general','lcd_update_sec') 
+lcd_update_sec = config.getint('general', 'lcd_update_sec')
 # ------------------------------------------------------------------------------------
 # Huawei Hi-Link GSM/3G USB dongle IP address on eth1
 # ------------------------------------------------------------------------------------
-hilink_device_ip = config.get('huawei', 'hilink_device_ip') 
+hilink_device_ip = config.get('huawei', 'hilink_device_ip')
 
 # ------------------------------------------------------------------------------------
 # I2C LCD: each I2C address will be tried in consecutive order until LCD is found
@@ -86,13 +86,13 @@ page = default_page
 
 sd_image_version = ''
 
-sshConfirm = False 
+sshConfirm = False
 shutConfirm = False
 
 # ------------------------------------------------------------------------------------
 # Start Logging
 # ------------------------------------------------------------------------------------
-uselogfile =  config.get('general','uselogfile') 
+uselogfile = config.get('general', 'uselogfile')
 logger = logging.getLogger("emonPiLCD")
 
 #ssh enable/disable/check commands
@@ -102,91 +102,77 @@ ssh_disable = "systemctl disable ssh > /dev/null"
 ssh_stop = "systemctl stop ssh > /dev/null"
 ssh_status = "systemctl status ssh > /dev/null"
 
-
-
-
-
 def buttonPressLong():
+    logger.info("Mode button LONG press")
 
-   logger.info("Mode button LONG press")
+    if sshConfirm:
+        ret = subprocess.call(ssh_status, shell=True)
+        if ret > 0:
+            #ssh not running, enable & start it
+            subprocess.call(ssh_enable, shell=True)
+            subprocess.call(ssh_start, shell=True)
+            logger.info("SSH Enabled")
+            lcd[0] = 'SSH Enabled      '
+            lcd[1] = 'Change password!'
+        else:
+            #disable ssh
+            subprocess.call(ssh_disable, shell=True)
+            subprocess.call(ssh_stop, shell=True)
+            logger.info("SSH Disabled")
+            lcd[0] = 'SSH Disabled      '
+            lcd[1] = '                '
 
-   if sshConfirm:
-
-      ret=subprocess.call(ssh_status, shell=True)
-      if ret > 0 :
-         #ssh not running, enable & start it
-         subprocess.call(ssh_enable, shell=True)
-         subprocess.call(ssh_start, shell=True)
-         logger.info("SSH Enabled")
-         lcd[0] = 'SSH Enabled      '
-         lcd[1] = 'Change password!'
-      else:
-         #disable ssh
-         subprocess.call(ssh_disable, shell=True)
-         subprocess.call(ssh_stop, shell=True)
-         logger.info("SSH Disabled")
-         lcd[0] = 'SSH Disabled      '
-         lcd[1] = '                '
-         
-
-   elif shutConfirm:
-      logger.info("Shutting down")
-      shutdown()
-   else:
-      lcd.backlight = not lcd.backlight
+    elif shutConfirm:
+        logger.info("Shutting down")
+        shutdown()
+    else:
+        lcd.backlight = not lcd.backlight
 
 
 def buttonPress():
-   global page
-   global lcd
-   global logger
+    global page
+    global lcd
+    global logger
 
-   now = time.time()
+    now = time.time()
 
-
-   if lcd.backlight:
+    if lcd.backlight:
         page += 1
-   if page > max_number_pages:
-    page = 0
-   buttonPress_time = now
-   if not lcd.backlight:
-    lcd.backlight = 1
-   logger.info("Mode button SHORT press")
-   logger.info("Page: " + str(page))
-   updateLCD() 
+    if page > max_number_pages:
+        page = 0
+    buttonPress_time = now
+    if not lcd.backlight:
+        lcd.backlight = 1
+    logger.info("Mode button SHORT press")
+    logger.info("Page: " + str(page))
+    updateLCD()
 
 
-def updateLCD() :
+def updateLCD():
+    global page
+    global lcd
+    global r
+    global logger
+    global sshConfirm
+    global shutConfirm
 
-   global page
-   global lcd
-   global r
-   global logger
-   global sshConfirm
-   global shutConfirm
+    # Create object for getting IP addresses of interfaces
+    ipaddress = IPAddress()
 
-   # Create object for getting IP addresses of interfaces
-   ipaddress = IPAddress()
+    if not page == 9:
+        sshConfirm = False
+    if not page == 11:
+        shutConfirm = False
 
-   if not page == 9:
-       sshConfirm = False
-   if not page == 11:
-       shutConfirm = False
-
-
-   # Now display the appropriate LCD page
-   if page == 0:
+    # Now display the appropriate LCD page
+    if page == 0:
     # Update ethernet
         eth0ip = ipaddress.get_ip_address('eth0')
-        active = 1
-        if eth0ip==0: active = 0
-        r.set("eth:active", active)
+        r.set("eth:active", int(bool(eth0ip)))
         r.set("eth:ip", eth0ip)
 
         wlan0ip = ipaddress.get_ip_address('wlan0')
-        active = 1
-        if wlan0ip==0: active = 0
-        r.set("wlan:active", active)
+        r.set("wlan:active", int(bool(wlan0ip)))
 
         if eval(r.get("eth:active")):
             lcd[0] = "Ethernet: YES"
@@ -197,13 +183,10 @@ def updateLCD() :
             lcd[0] = "Ethernet:"
             lcd[1] = "NOT CONNECTED"
 
-   if page == 1:
-   # Update wifi
+    if page == 1:
+    # Update wifi
         wlan0ip = ipaddress.get_ip_address('wlan0')
-        active = 1
-        if wlan0ip==0: active = 0
-		
-        r.set("wlan:active", active)
+        r.set("wlan:active", int(bool(wlan0ip)))
         r.set("wlan:ip", wlan0ip)
 
         signallevel = 0
@@ -215,18 +198,17 @@ def updateLCD() :
             if signals:
                 signallevel = signals[0].partition('.')[0]
             if str(signallevel).startswith('-'): # Detect the alternate signal strength reporting via dBm
-                signallevel = 2 * ( int(signallevel) + 100 ) # Convert to percent
+                signallevel = 2 * (int(signallevel) + 100) # Convert to percent
             r.set("wlan:signallevel", signallevel)
-
 
         if eval(r.get("wlan:active")):
             if int(r.get("wlan:signallevel")) > 0:
-               lcd[0] = "WiFi: YES  " + r.get("wlan:signallevel") + "%"
+                lcd[0] = "WiFi: YES  " + r.get("wlan:signallevel") + "%"
             else:
-               if r.get("wlan:ip") == "192.168.42.1":
-                  lcd[0] = "WiFi: AP MODE"
-               else:
-                  lcd[0] = "WiFi: YES  "
+                if r.get("wlan:ip") == "192.168.42.1":
+                    lcd[0] = "WiFi: AP MODE"
+                else:
+                    lcd[0] = "WiFi: YES  "
 
             lcd[1] = r.get("wlan:ip")
         elif eval(r.get("gsm:active")) or eval(r.get("eth:active")):
@@ -235,7 +217,7 @@ def updateLCD() :
             lcd[0] = "WiFi:"
             lcd[1] = "NOT CONNECTED"
 
-   if page == 2:
+    if page == 2:
     # Update Hi-Link 3G Dongle - connects on eth1
         if ipaddress.get_ip_address("eth1") and gsmhuaweistatus.is_hilink(hilink_device_ip):
             gsm_connection_status = gsmhuaweistatus.return_gsm_connection_status(hilink_device_ip)
@@ -244,7 +226,6 @@ def updateLCD() :
             r.set("gsm:active", 1)
         else:
             r.set("gsm:active", 0)
-
 
         if eval(r.get("gsm:active")):
             lcd[0] = r.get("gsm:connection")
@@ -255,29 +236,29 @@ def updateLCD() :
             lcd[0] = "GSM:"
             lcd[1] = "NO DEVICE"
 
-   if page == 3:
+    if page == 3:
         if r.get("feed1") is not None:
-            lcd[0] = feed1_name + ':'  + r.get("feed1") + feed1_unit 
+            lcd[0] = feed1_name + ':'  + r.get("feed1") + feed1_unit
         else:
             lcd[0] = feed1_name + ':'  + "---"
 
         if r.get("feed2") is not None:
-            lcd[1] = feed2_name + ':'  + r.get("feed2") + feed2_unit 
+            lcd[1] = feed2_name + ':'  + r.get("feed2") + feed2_unit
         else:
             lcd[1] = feed2_name + ':'  + "---"
 
-   elif page == 4:
+    elif page == 4:
         basedata = r.get("basedata")
         if basedata is not None:
             basedata = basedata.split(",")
             lcd[0] = 'VRMS: ' + basedata[3] + "V"
-	    lcd[1] = 'Pulse: ' + basedata[10] + "p"
+            lcd[1] = 'Pulse: ' + basedata[10] + "p"
         else:
             lcd[0] = 'Connecting...'
             lcd[1] = 'Please Wait'
-            page +=1
+            page += 1
 
-   elif page == 5:
+    elif page == 5:
         basedata = r.get("basedata")
         if basedata is not None:
             basedata = basedata.split(",")
@@ -286,9 +267,9 @@ def updateLCD() :
         else:
             lcd[0] = 'Connecting...'
             lcd[1] = 'Please Wait'
-            page +=1
+            page += 1
 
-   elif page == 6:
+    elif page == 6:
     # Get uptime
         with open('/proc/uptime', 'r') as f:
             seconds = float(f.readline().split()[0])
@@ -297,33 +278,32 @@ def updateLCD() :
         lcd[0] = datetime.now().strftime('%b %d %H:%M')
         lcd[1] = 'Uptime %.2f days' % (seconds / 86400)
 
-   elif page == 7:
+    elif page == 7:
         lcd[0] = "emonPi Build:"
         lcd[1] = sd_image_version
 
-   elif page == 8:
-        ret=subprocess.call(ssh_status, shell=True)
-        if ret > 0 : 
-          #ssh not running
-          lcd[0] = "SSH Enable?"
+    elif page == 8:
+        ret = subprocess.call(ssh_status, shell=True)
+        if ret > 0:
+            #ssh not running
+            lcd[0] = "SSH Enable?"
         else:
-          #ssh not running
-          lcd[0] = "SSH Disable?"
+            #ssh not running
+            lcd[0] = "SSH Disable?"
 
         lcd[1] = "Y press & hold"
-	sshConfirm = False
+        sshConfirm = False
 
-   elif page == 9:
+    elif page == 9:
         sshConfirm = True
 
-   elif page == 10:
+    elif page == 10:
         lcd[0] = "Shutdown?"
         lcd[1] = "Y press & hold"
-	shutConfirm = False
-   elif page == 11:
+        shutConfirm = False
+    elif page == 11:
         lcd[0] = "Shutdown?"
-	shutConfirm = True
-
+        shutConfirm = True
 
 
 class IPAddress(object):
@@ -363,26 +343,26 @@ def shutdown():
 
 class LCD(object):
     def __init__(self, logger):
-        # Scan I2C bus for LCD I2C addresses as defined in led_i2c, we have a couple of models of LCD which have different adreses that are shipped with emonPi. First I2C device to match address is used.
+        # Scan I2C bus for LCD I2C addresses as defined in led_i2c, we have a couple of models of LCD which have different addreses that are shipped with emonPi. First I2C device to match address is used.
         self.logger = logger
         for i2c_address in lcd_i2c:
-          lcd_status = subprocess.check_output([path+"/emonPiLCD_detect.sh", "%s" % i2c_address])
-          if lcd_status.rstrip() == 'True':
-            print "I2C LCD DETECTED Ox%s" % i2c_address
-            logger.info("I2C LCD DETECTED 0x%s" % i2c_address)
-            current_lcd_i2c = "0x%s" % i2c_address
-            # add file to identify device as emonpi
-            if os.path.isdir('/home/pi/data/'):
-                open('/home/pi/data/emonpi', 'a').close()
-            break
+            lcd_status = subprocess.check_output([path+"/emonPiLCD_detect.sh", "%s" % i2c_address])
+            if lcd_status.rstrip() == 'True':
+                print "I2C LCD DETECTED Ox%s" % i2c_address
+                logger.info("I2C LCD DETECTED 0x%s" % i2c_address)
+                current_lcd_i2c = "0x%s" % i2c_address
+                # add file to identify device as emonpi
+                if os.path.isdir('/home/pi/data/'):
+                    open('/home/pi/data/emonpi', 'a').close()
+                break
 
         if lcd_status.rstrip() == 'False':
-          print ("I2C LCD NOT DETECTED on either 0x" + str(lcd_i2c) + " ...exiting LCD script")
-          logger.error("I2C LCD NOT DETECTED on either 0x" + str(lcd_i2c) + " ...exiting LCD script")
-          # add file to identify device as emonbase
-          if os.path.isdir('/home/pi/data/'):
-              open('/home/pi/data/emonbase', 'a').close()
-          sys.exit(0)
+            print("I2C LCD NOT DETECTED on either 0x" + str(lcd_i2c) + " ...exiting LCD script")
+            logger.error("I2C LCD NOT DETECTED on either 0x" + str(lcd_i2c) + " ...exiting LCD script")
+            # add file to identify device as emonbase
+            if os.path.isdir('/home/pi/data/'):
+                open('/home/pi/data/emonbase', 'a').close()
+            sys.exit(0)
 
         # Init LCD using detected I2C address with 16 characters
         self.lcd = lcddriver.lcd(int(current_lcd_i2c, 16))
@@ -428,7 +408,7 @@ def main():
         loghandler = logging.StreamHandler()
     else:
         logfile = "/var/log/emonpilcd/emonpilcd.log"
-        print "emonPiLCD logging to: "+logfile
+        print "emonPiLCD logging to:", logfile
         loghandler = logging.handlers.RotatingFileHandler(logfile,
                                                           mode='a',
                                                           maxBytes=1000 * 1024,
@@ -442,7 +422,7 @@ def main():
     logger.info("Starting emonPiLCD V" + version)
 
     # Now check the LCD and initialise the object
-    global lcd 
+    global lcd
     lcd = LCD(logger)
     lcd.backlight = 1
 
@@ -458,7 +438,7 @@ def main():
         if not sd_card_image:
             sd_image_version = subprocess.check_output("ls /boot | grep emonpi", shell=True)
         else:
-            sd_image_version = "N/A "
+            sd_image_version = "N/A"
     sd_image_version = sd_image_version.rstrip()
 
     lcd[0] = "emonPi Build:"
@@ -473,20 +453,19 @@ def main():
     # No bounce time increases responce time but may result in switch bouncing...
     logger.info("Attaching push button interrupt...")
     try:
-       push_btn = Button(23, pull_up=False, hold_time=5)
-       push_btn.when_pressed = buttonPress
-       push_btn.when_held = buttonPressLong
-    except: 
-       logger.error("Failed to attach LCD push button interrupt...")
+        push_btn = Button(23, pull_up=False, hold_time=5)
+        push_btn.when_pressed = buttonPress
+        push_btn.when_held = buttonPressLong
+    except:
+        logger.error("Failed to attach LCD push button interrupt...")
 
     logger.info("Attaching shutdown button interrupt...")
     try:
-       shut_btn = Button(17, pull_up=False, hold_time=5)
-       shut_btn.when_pressed = preShutdown
-       shut_btn.when_held = shutdown 
-    except: 
-       logger.error("Failed to attach shutdown button interrupt...")
-
+        shut_btn = Button(17, pull_up=False, hold_time=5)
+        shut_btn.when_pressed = preShutdown
+        shut_btn.when_held = shutdown
+    except:
+        logger.error("Failed to attach shutdown button interrupt...")
 
     logger.info("Connecting to redis server...")
     # We wait here until redis has successfully started up
@@ -502,20 +481,18 @@ def main():
     logger.info("Connecting to MQTT Server: " + mqtt_host + " on port: " + str(mqtt_port) + " with user: " + mqtt_user)
 
     def on_message(client, userdata, msg):
-        topic_parts = msg.topic.split("/")
-
-        if mqtt_feed1_topic in msg.topic: 
-            r.set("feed1" , msg.payload)
+        if mqtt_feed1_topic in msg.topic:
+            r.set("feed1", msg.payload)
 
         if mqtt_feed2_topic in msg.topic:
-            r.set("feed2" , msg.payload)
+            r.set("feed2", msg.payload)
 
         if mqtt_emonpi_topic in msg.topic:
             r.set("basedata", msg.payload)
 
 
     def on_connect(client, userdata, flags, rc):
-        if (rc==0):
+        if rc == 0:
             mqttc.subscribe(mqtt_emonpi_topic)
             mqttc.subscribe(mqtt_feed1_topic)
             mqttc.subscribe(mqtt_feed2_topic)
@@ -540,24 +517,21 @@ def main():
 
     buttonPress_time = time.time()
 
-
-    if not backlight_timeout: 
-      lcd.backlight = 1
+    if not backlight_timeout:
+        lcd.backlight = 1
 
     page = default_page
 
     # Enter main loop
     while True:
-
-        # turn backight off after backlight_timeout seconds
+        # turn backlight off after backlight_timeout seconds
         now = time.time()
-        if (backlight_timeout) and now - buttonPress_time > backlight_timeout and lcd.backlight:
+        if backlight_timeout and now - buttonPress_time > backlight_timeout and lcd.backlight:
             lcd.backlight = 0
-        
+
         #Update LCD in case it is left at a screen where values can change (e.g uptime etc)
-        updateLCD() ;
-        time.sleep(lcd_update_sec) 
+        updateLCD()
+        time.sleep(lcd_update_sec)
 
 if __name__ == '__main__':
     main()
-
