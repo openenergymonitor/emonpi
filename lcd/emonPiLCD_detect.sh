7ca0f58ab412ef3fb7721fa4a5c750f614239a76
Bruce Duncan <bwduncan@gmail.com>
2019-10-17 20:51:32 +0100

    Make lcd detect script more robust.

--- lcd/emonPiLCD_detect.sh
+++ lcd/emonPiLCD_detect.sh
@@ -10,16 +10,14 @@
 # sudo ./emonPiLCD_detect.sh 27 2
 # Add a 2 as a second argument to search I2C bus 2, default 1
 
-bus=1
-if [[ -n $2 ]]; then
-    bus=$2
-fi
+address=${1:?missing address}
+bus=${2:-1}
 
 mapfile -t data < <(i2cdetect -y "$bus")
 
 for i in $(seq 1 ${#data[@]}); do
     line=(${data[$i]})
-    if echo ${line[@]:1} | grep -q $1; then
+    if echo "${line[@]:1}" | grep -q "$address"; then
         echo "True"
         exit 0
     fi
