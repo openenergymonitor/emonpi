7d12643b5741240bf49a45f768c16550d6117b86
Trystan Lea <trystan.lea@googlemail.com>
2020-08-03 23:03:22 +0100

    Update emonPiLCD_update.py

--- lcd/emonPiLCD_update.py
+++ lcd/emonPiLCD_update.py
@@ -1,34 +1,22 @@
 #!/usr/bin/env python
-
-from __future__ import print_function
+import lcddriver
+import time
 import sys
-import subprocess
-import os
+import smbus
 
-dir = os.path.dirname(os.path.realpath(__file__))
+# Find LCD (alternative to i2cdetect)
+bus = smbus.SMBus(1)
+for address in ['0x27','0x3f']:
+    try:
+        bus.read_byte(int(address,16))
+        break
+    except Exception:
+        address=False
 
-# ------------------------------------------------------------------------------------
-# Check to see if LCD is connected if not then stop here
-# ------------------------------------------------------------------------------------
-lcd_i2c = ['27', '3f']
-current_lcd_i2c = ''
+if not address:
+    sys.exit()
 
-for i2c_address in lcd_i2c:
-  lcd_status = subprocess.check_output([dir+"/emonPiLCD_detect.sh", "%s" % i2c_address])
-  if lcd_status.rstrip() == 'True':
-    print("I2C LCD DETECTED Ox%s" % i2c_address)
-    current_lcd_i2c = "0x%s" % i2c_address
-    break
-
-if lcd_status.rstrip() == 'False':
-    print("I2C LCD NOT DETECTED on either 0x" + str(lcd_i2c) + " ...exiting LCD script")
-    sys.exit(1)
-
-lcd = lcddriver.lcd(int(current_lcd_i2c, 16))
-
-# ------------------------------------------------------------------------------------
-# Display update in progress update
-# ------------------------------------------------------------------------------------
+lcd = lcddriver.lcd(int(address, 16))
 lcd.backlight = 1
 lcd.lcd_display_string("Updating........", 1)
 lcd.lcd_display_string("DO NOT UNPLUG!  ", 2)
