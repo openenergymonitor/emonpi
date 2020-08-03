842ef4c6fed65443d2c28ec95bf6c89c60dc7d57
TrystanLea <trystan.lea@gmail.com>
2020-08-03 09:16:18 +0100

    alternative scripts to detect and write content to emonPiLCD

---
+++ lcd/emonPiLCD_detect.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+import sys
+import smbus
+
+# Find LCD (alternative to i2cdetect)
+bus = smbus.SMBus(1)
+for address in ['0x27','0x3f']:
+    try:
+        bus.read_byte(int(address,16))
+        break
+    except Exception:
+        address=False
+        
+print address
---
+++ lcd/emonPiLCD_write.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+import lcddriver
+import time
+import sys
+import smbus
+
+# Find LCD (alternative to i2cdetect)
+bus = smbus.SMBus(1)
+for address in ['0x27','0x3f']:
+    try:
+        bus.read_byte(int(address,16))
+        break
+    except Exception:
+        address=False
+
+if not address:
+    sys.exit()
+
+if len(sys.argv)!=3:
+    print "not enough arguments provided, expecting: line1, line2"
+    sys.exit()
+
+line1 = sys.argv[1]
+line2 = sys.argv[2]
+  
+lcd = lcddriver.lcd(int(address, 16))
+lcd.backlight = 1
+lcd.lcd_display_string(line1,1)
+lcd.lcd_display_string(line2,2)
