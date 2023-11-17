41ce9e3d1b7be97045bfce0401ce55dfd498c4b9
TrystanLea <trystan.lea@gmail.com>
2023-11-17 13:01:06 +0000

    support for both emonPi versions

--- lcd/emonPiLCD_detect.py
+++ lcd/emonPiLCD_detect.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 import sys
 import smbus
+import emonPiLCD1
+import emonPiLCD2
 
 # Find LCD (alternative to i2cdetect)
 bus = smbus.SMBus(1)
-for address in ['0x27','0x3f']:
+for address in ['0x27','0x3f','0x3c']:
     try:
-        bus.read_byte(int(address,16))
+        print(bus.read_byte(int(address,16)))
         break
     except Exception:
         address=False
